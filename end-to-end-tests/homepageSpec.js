ba6e169be1043f88ce5482eddd55a065648e9ba2
pauldambra <paul.dambra@gmail.com>
2014-03-25 22:33:10 +0000

    remove comments from spec

--- end-to-end-tests/homepageSpec.js
+++ end-to-end-tests/homepageSpec.js
@@ -3,10 +3,6 @@ module.exports = {
     browser
       .url("http://omniclopse-v0-1.herokuapp.com/")
       .waitForElementVisible('body', 1000)
-      //.setValue('input[type=text]', 'nightwatch')
-      //.click('button[name=btnG]')
-      //.pause(1000)
-      //.assert.containsText('#main', 'The Night Watch')
       .assert.elementPresent('#homeCarousel')
       //must have at least one image
       .assert.elementPresent('#homeCarousel .item img')
