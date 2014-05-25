c2ebca9094836c6b40a440d73682a729d8c2a411
pauldambra <paul.dambra@gmail.com>
2014-05-25 15:13:50 +0100

    little tidying up

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require('express');
-var app = express();
+var app;
 var passport = require('passport');
-var flash = require('connect-flash');
+
 var hbs = require('hbs');
 var login = require('./server/login');
 var db = require('./server/db').db;
@@ -10,63 +10,9 @@ passport.use(login.localStrategy);
 passport.serializeUser(login.serializeUser);
 passport.deserializeUser(login.deserializeUser);
 
-app.locals({
-        user:null
-    });
+var app = require('./server/config').initApp(__dirname);
 
-app.use(express.logger('dev'));     /* 'default', 'short', 'tiny', 'dev' */
-app.use(express.json());
-app.use(express.urlencoded());
-app.use(express.cookieParser('omniclopse cookie'));
-app.use(express.session({ 
-    secret: 'ilovescotchscotchyscotchscotch',
-    cookie: {maxAge: new Date(Date.now() + 3600000)}, // 1 hour
-    maxAge: new Date(Date.now() + 3600000) // 1 hour 
-}));
-app.use(passport.initialize());
-app.use(passport.session()); // persistent login sessions
-app.use('/libs', express.static(__dirname + '/bower_components'));
-app.use(express.static(__dirname + '/public'));
-app.use(flash());
-
-hbs.registerHelper('markActiveWhenMatchesCarouselStartIndex', 
-                    function(index) {
-                        return index == 2 ? 'active' : '';
-                    });
-hbs.registerHelper('loginBlock', 
-                    function(user) {
-                        return user
-                            ? '<a href="/logout">Logged in as ' + user + ' - Log out</a>'
-                            : '<a href="/login">Login</a>';
-                    });
-hbs.registerHelper('elementShouldBeEditable', 
-                    function(user) {
-                        if (app.locals.user) {
-                            return "contenteditable=true";
-                        }
-                    });
-
-var blocks = {};
-
-hbs.registerHelper('extend', function(name, context) {
-    var block = blocks[name];
-    if (!block) {
-        block = blocks[name] = [];
-    }
-
-    block.push(context.fn(this)); // for older versions of handlebars, use block.push(context(this));
-});
-
-hbs.registerHelper('block', function(name) {
-    var val = (blocks[name] || []).join('\n');
-
-    // clear the block
-    blocks[name] = [];
-    return val;
-});
-
-app.set('view engine', 'html');
-app.set('views', __dirname + '/views');
+require('./server/handlebars').init(hbs, app.locals);
 hbs.registerPartials(__dirname + '/views/partials');
 app.engine('html', hbs.__express);
 
