dd5fd398634a9c20fa0c2e130ca6e83083aad47c
pauldambra <paul.dambra@gmail.com>
2014-07-14 22:33:06 +0100

    add cleaning of app.js to build

--- gulpfile.js
+++ gulpfile.js
@@ -3,8 +3,14 @@ var sass = require('gulp-sass');
 var nodemon = require('gulp-nodemon');
 var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
+var rimraf = require('gulp-rimraf');
 
-gulp.task('processJS', function(){
+gulp.task('clean', function() {
+  gulp.src('./public/js/app.js', {read:false})
+    .pipe(rimraf());
+});
+
+gulp.task('processJS', ['clean'], function(){
   gulp.src(['./public/js/*.js'])
     .pipe(concat('app.js'))
     .pipe(uglify())
